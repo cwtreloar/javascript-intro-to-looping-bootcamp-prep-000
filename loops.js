5cffeccd39333db754370a907f1301920a4154b0
Charles Treloar <cwtreloar@gmail.com>
2018-12-13 15:48:31 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array) {
+  for (let i = array; i < 25; i++) {
+    
+    if (i == 0) {
+      return `I am ${i} strange loop.`;
+    }
+    
+    else {
+      return `I am ${i} strange loops.`;
+    }
+  }
+}
